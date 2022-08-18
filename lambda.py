6d999ec5e79fd7f8565c075dd37118edb07110a0
YASSER KARAMI <83478271+karami4yasser@users.noreply.github.com>
2022-08-18 18:49:10 +0100

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -1,5 +1,7 @@
 import json
 import boto3
+import csv
+import io
 
 def elastic_ips_cleanup():
     client = boto3.client('ec2')
@@ -11,5 +13,22 @@ def elastic_ips_cleanup():
     return unUsed_Ips
             
 def lambda_handler(event, context):
+    s3 = boto3.client('s3')
+
     unUsed_Ips=elastic_ips_cleanup()
+    csvio = io.StringIO()
+    writer = csv.writer(csvio)
+    writer.writerow([
+        'IP'
+    ])
+    for elem in unUsed_Ips:
+        writer.writerow([
+            elem
+            ])
+
+    s3.put_object(Body=csvio.getvalue(), ContentType='application/vnd.ms-excel', Bucket='YOURBUCKETNAME', Key='unUsed_Ips.csv') 
+    csvio.close()
+
+    
     return unUsed_Ips
+
